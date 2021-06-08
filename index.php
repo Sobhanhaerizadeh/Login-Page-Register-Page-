d42ded85fe1336800272a33aa0b4d529a149a701
Sobhan haerizadeh <sobhanhaerizade@gmail.com>
2021-06-08 14:55:36 +0430

    New files

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/bootstrap.min.css"/>
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/style.css"/>
+<title>Login Page</title>
+</head>
+<body>
+<div class="container">
+    <form class="form-login">
+    <label for="input-name">Nickname </label>
+    <input type="text" id="input-name" name="input-name" class="form-control" required autofocus/> <br/>
+    <label for="input-password"> Password </label>
+    <input type="password"  id="input-password" name="input-password" class="form-control" required/> <br/>
+    <input type="submit" value="Login" class="btn btn-success" />
+    <input type="reset" value="Reset" class="btn btn-danger" />
+    </form>
+</div>
+</body>
+</html>
